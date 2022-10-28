1506ac71a7fc695d3908e30476491020624e7101
Can Özçelik <canozcelik1@gmail.com>
2022-10-28 17:06:25 +0300

    initial commit

---
+++ src/main/java/co/upstart/assignment/newsapi/NewsApiApplication.java
@@ -0,0 +1,13 @@
+package co.upstart.assignment.newsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/upstart/assignment/newsapi/NewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.upstart.assignment.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
