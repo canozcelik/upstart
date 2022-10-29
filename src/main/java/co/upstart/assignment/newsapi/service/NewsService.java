4fd1871a8bdac89db47d264f25be08e232aca6ff
Can Özçelik <canozcelik1@gmail.com>
2022-10-30 01:27:17 +0300

    News service was added

---
+++ src/main/java/co/upstart/assignment/newsapi/service/NewsService.java
@@ -0,0 +1,9 @@
+package co.upstart.assignment.newsapi.service;
+
+import co.upstart.assignment.newsapi.domain.dto.response.Response;
+
+public interface NewsService {
+
+    Response getNews(String query);
+
+}
