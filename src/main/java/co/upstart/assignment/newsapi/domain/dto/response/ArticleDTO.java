8f0be069b2fa59fa4e9515ffe442ad1d8b1d8ef6
Can Özçelik <canozcelik1@gmail.com>
2022-11-01 10:34:08 +0300

    docker file renamed. LocalDateTimeDeserializer was added for LocalDateTime

--- src/main/java/co/upstart/assignment/newsapi/domain/dto/response/ArticleDTO.java
+++ src/main/java/co/upstart/assignment/newsapi/domain/dto/response/ArticleDTO.java
@@ -1,5 +1,9 @@
 package co.upstart.assignment.newsapi.domain.dto.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +18,8 @@ public class ArticleDTO {
     private String content;
     private String url;
     private String image;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime publishDate;
     private SourceDTO source;
 }
--- src/main/java/co/upstart/assignment/newsapi/service/impl/NewsServiceImpl.java
+++ src/main/java/co/upstart/assignment/newsapi/service/impl/NewsServiceImpl.java
@@ -5,12 +5,15 @@ import co.upstart.assignment.newsapi.domain.dto.response.mapper.ResponseMapper;
 import co.upstart.assignment.newsapi.newsclient.NewsClient;
 import co.upstart.assignment.newsapi.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames ="news-api")
 public class NewsServiceImpl implements NewsService {
 
     private static Pattern regex = Pattern.compile("[^A-Za-z0-9]");
@@ -19,6 +22,7 @@ public class NewsServiceImpl implements NewsService {
     private final ResponseMapper responseMapper;
 
     @Override
+    @Cacheable(cacheNames = "news")
     public Response getNews(String query, int size) {
         return responseMapper.toResponse(newsClient.getArticles(validateQuery(query), size));
     }
